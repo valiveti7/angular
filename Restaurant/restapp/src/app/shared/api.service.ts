b42f5f9b899e5db5c9a85c220f6e4d0ded354737
Valiveti Sri Ram Kumar <96459445+valiveti7@users.noreply.github.com>
2022-04-04 09:19:16 +0530

    Add files via upload

---
+++ Restaurant/restapp/src/app/shared/api.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private _http:HttpClient) { }
+
+  postRestaurant(data:any){
+    return this._http.post<any>('http://localhost:3000/posts',data).pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  getrestaurant(){
+    return this._http.get<any>('http://localhost:3000/posts').pipe(map((res:any)=>{
+      return res;
+  }))
+  }
+  updaterestaurant(data:any,id:number){
+    return this._http.put<any>('http://localhost:3000/posts/'+id,data).pipe(map((res:any)=>{
+      return res;
+  }))
+  }
+  deleterestaurant(id:number){
+    return this._http.delete<any>('http://localhost:3000/posts/'+id).pipe(map((res:any)=>{
+      return res;
+  }))
+  }
+}
